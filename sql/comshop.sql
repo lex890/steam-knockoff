597649ac177c19a369e1d5d7f7ae0cd8228fe729
lex890 <ashketchopstick@gmail.com>
2024-07-09 11:04:51 +0800

    huh

--- sql/comshop.sql
+++ sql/comshop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 09, 2024 at 04:09 AM
+-- Generation Time: Jul 09, 2024 at 04:49 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
